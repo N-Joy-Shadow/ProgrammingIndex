3dc42cefcb2fc20acedbbb9e9dc2ce21aa274611
N-Joy-Shadow <shs3740413@gmail.com>
2023-05-23 21:30:06 +0900

    commit

---
+++ KumohProgramming/Utils/DB/DatabaseConnector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MongoDB;
+namespace KumohProgramming.Utils.DB
+{
+	public class DatabaseConnector
+	{
+
+		public DatabaseConnector()
+		{
+            var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+
+        }
+    }
+}

99dc23330c9146d84a297cfd24ef4ba6956717e1
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2023-05-24 01:27:27 +0900

    add env

--- KumohProgramming/Utils/DB/DatabaseConnector.cs
+++ KumohProgramming/Utils/DB/DatabaseConnector.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using MongoDB;
+using MongoDB.Driver;
+
 namespace KumohProgramming.Utils.DB
 {
 	public class DatabaseConnector
 	{
-
+        DatabaseConnector Instance = new DatabaseConnector();
 		public DatabaseConnector()
 		{
-            var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+            string connectionString = Env.Get["MongoDB_URI"];
+
+             var client = new MongoClient(connectionString);
+
 
         }
     }
---
+++ KumohProgramming/Utils/Env.cs
@@ -0,0 +1,16 @@
+﻿namespace KumohProgramming.Utils
+{
+    public class Env
+    {
+        public static bool IsDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" ? true : false;
+        public static bool IsProduction = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production" ? true : false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IConfiguration Get = new ConfigurationBuilder()
+            .AddJsonFile(IsDevelopment ? "appsettings.Development.json" : "appsettings.json", optional: true)
+            .Build();
+
+    }
+}
